5f1fe7584894bcd2974f023169034a417608a3f9
jaafar <jaafar.tour.travel@gmail.com>
2023-05-22 15:20:42 +0200

    first commi111t

---
+++ jafardata/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'z22pgkc6',
+    dataset: 'production'
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,12 @@
+import { SanityClient } from "@sanity/client";
+
+
+export default SanityClient(  
+
+    {
+        projectId: 'z22pgkc6',
+        dataset: 'production'
+
+
+    }
+)
